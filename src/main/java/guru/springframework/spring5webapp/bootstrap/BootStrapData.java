3e9bdf34a94777ef59f7387d944077ee10cdd4f5
ANTOINE NOUVENE <anouvene@ANTOINEs-MacBook-Pro.local>
2020-05-16 22:06:36 +0200

    Added Publisher entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,23 +2,28 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 /**
- * Created by jt on 12/23/19.
+ * Created by antoine nouvene on 16/05/20.
  */
 @Component
 public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -43,5 +48,11 @@ public class BootStrapData implements CommandLineRunner {
         bookRepository.save(noEJB);
 
         System.out.println("Number of Books: " + bookRepository.count());
+
+        Publisher hachetteLivre;
+        hachetteLivre = new Publisher("Hachette Livre", "58, rue Jean Bleuzen", "Vanves",
+                "Hauts-de-Seine", "92170");
+        this.publisherRepository.save(hachetteLivre);
+        System.out.println("Number of editors: " +  publisherRepository.count());
     }
 }
